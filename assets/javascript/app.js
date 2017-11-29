cf8918eef17015ae8582182ea3cf667cc241d1c4
Louella Landicho <louellalandicho@Louellas-MacBook-Pro.local>
2017-11-29 14:04:05 -0800

    index and js working plain so far

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 
 
+
 var myQuestions = [
     {
         question: "What is 10/2?",
@@ -106,5 +107,44 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
     submitButton.onclick = function(){
         showResults(questions, quizContainer, resultsContainer);
     }
+
+
+var number = 5;
+    //  Variable that will hold our interval ID when we execute
+    //  the "run" function
+    var intervalId;
+    //  When the stop button gets clicked, run the stop function.
+    $("#start").on("click", start);
+    //  When the resume button gets clicked, execute the run function.
+    
+    //  The run function sets an interval
+    //  that runs the decrement function once a second.
+    function run() {
+      intervalId = setInterval(decrement, 1000);
+    }
+    //  The decrement function.
+    function decrement() {
+      //  Decrease number by one.
+      number--;
+      //  Show the number in the #show-number tag.
+      $("#show-number").html("<h2>" + number + "</h2>");
+      //  Once number hits zero...
+      if (number === 0) {
+        //  ...run the stop function.
+        stop();
+        //  Alert the user that time is up.
+        alert("Time Up!");
+      }
+    }
+    //  The stop function
+    function stop() {
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+    //  Execute the run function.
+    run();
+
 
 }
